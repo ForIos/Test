df6831f7b29366796c4465260f5f9d04758f2baf
USER <894143788@qq.com>
2016-06-15 13:05:45 +0800

    test

---
+++ TestOnly/TestOnly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestOnly
+//
+//  Created by USER on 16/6/15.
+//  Copyright © 2016年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TestOnly/TestOnlyTests/TestOnlyTests.m
@@ -0,0 +1,39 @@
+//
+//  TestOnlyTests.m
+//  TestOnlyTests
+//
+//  Created by USER on 16/6/15.
+//  Copyright © 2016年 USER. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestOnlyTests : XCTestCase
+
+@end
+
+@implementation TestOnlyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
